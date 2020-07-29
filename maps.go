d284eb01e2070530f601da8c399ab52b5078b6bf
petros <pit.trak@gmail.com>
2020-07-29 20:05:56 +0300

    Maps in Go

---
+++ maps.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// statePopulation := make(map[string]int)
+	// statePopulation = map[string]int{
+	statePopulation := map[string]int{
+		"California": 39260012,
+		"Texas":      27862596,
+		"Florida":    20612439,
+		"New York":   19745289,
+		"Ohio":       11614373,
+	}
+	fmt.Println(statePopulation)
+	statePopulation["Georgia"] = 10310371
+	fmt.Println(statePopulation)
+	delete(statePopulation, "New York")
+	fmt.Println(statePopulation)
+	fmt.Println(statePopulation["New York"])
+	_, ok := statePopulation["New York"]
+	fmt.Println(ok)
+	_, ok = statePopulation["Ohio"]
+	fmt.Println(ok)
+	fmt.Println(len(statePopulation))
+}
