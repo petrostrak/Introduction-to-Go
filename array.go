3a2a82f8ccfd6bb5dd64ff79887314c886b8b72a
petros <pit.trak@gmail.com>
2020-07-29 17:33:23 +0300

    Arrays in Go

---
+++ array.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	grades := [...]int{97, 68, 35}
+	fmt.Printf("Grades : %v\n", grades)
+
+	fmt.Println("***")
+
+	a := [...]int{1, 2, 3}
+	b := a // Assigning an variable to an array, will not point to it, instear it will copy the array.
+	// b := &a // To point to an array without coping, we use the & operator
+	b[1] = 5
+	fmt.Println("Array a :", a)
+	fmt.Println("Array b :", b, " after coping (declaring) b = a.")
+
+	fmt.Println("***")
+
+	var identityMatrix [3][3]int
+	identityMatrix[0] = [3]int{1, 0, 0}
+	identityMatrix[1] = [3]int{0, 1, 0}
+	identityMatrix[2] = [3]int{0, 0, 1}
+	fmt.Printf("Identity Matrix : %v\n", identityMatrix)
+}
