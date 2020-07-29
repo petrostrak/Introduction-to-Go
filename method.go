e9e15f05c473af57a8252e27039f5514bdda710d
petros <pit.trak@gmail.com>
2020-07-30 00:09:06 +0300

    Method in Go

---
+++ method.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	g := greeter{
+		greeting: "Hello",
+		name:     "Go!",
+	}
+	g.greet()
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g greeter) greet() {
+	fmt.Println(g.greeting, g.name)
+}
