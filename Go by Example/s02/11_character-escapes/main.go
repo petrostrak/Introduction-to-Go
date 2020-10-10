e433a3af9983db79b7735c2a0d4dbf8d670a6237
petros <pit.trak@gmail.com>
2020-10-10 12:53:06 +0300

    Go character escapes

---
+++ Go by Example/s02/11_character-escapes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("col1\tcol2\tcol3")
+	fmt.Print("- - - - - - - - - - - - \n")
+	fmt.Println("item11\titem12\titem13")
+	fmt.Println("item21\t\titem23")
+
+	fmt.Printf("\n%s\t%s\t%s", "col1", "col2", "col3")
+	fmt.Print("\n- - - - - - - - - - - -")
+	fmt.Printf("\n%s\t%s\t%s", "item11", "item12", "item13")
+	fmt.Printf("\n%s\t%s\t%s", "item21", "", "item23")
+}
