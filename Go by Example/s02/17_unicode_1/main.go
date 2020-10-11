f7183c090b38f2bf1260f1e319d1acc7679e575b
petros <pit.trak@gmail.com>
2020-10-11 12:55:26 +0300

    Go unicode

---
+++ Go by Example/s02/17_unicode_1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ascii := 'a'
+	unicode := '㪑'
+	newline := '\n'
+
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)
+	fmt.Printf("%d %[1]c %[1]q\n", unicode)
+	fmt.Printf("%d %[1]c %[1]q\n", newline)
+
+}
