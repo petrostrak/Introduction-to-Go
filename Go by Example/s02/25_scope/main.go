ae71b953fbda7e5c5b13d5835b9d6655188c8c5f
petros <pit.trak@gmail.com>
2020-10-11 17:02:32 +0300

    Go scopes

---
+++ Go by Example/s02/25_scope/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+var g int = 10
+
+// g := 20
+
+func main() {
+
+	fmt.Println(g)
+
+	{
+		g := 20
+		fmt.Println(g)
+		f := 2.3
+		fmt.Println(f)
+	}
+
+	// fmt.Println(f) //compiler error - undefined 'f'
+
+	fmt.Println(g)
+	g += 1
+
+	{
+		g += 4
+		fmt.Println(g)
+	}
+
+	sayHello()
+	// fmt.Println(s)		//compiler error - undefined 's'
+}
+
+func sayHello() {
+	fmt.Println("Hello!")
+	fmt.Println(g)
+
+	s := "from testScope()"
+	fmt.Println(s)
+}
