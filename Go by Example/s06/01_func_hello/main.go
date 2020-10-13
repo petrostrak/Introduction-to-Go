f559b4f760c62d404fe52462c5c9362af7659ce3
petros <pit.trak@gmail.com>
2020-10-13 17:10:32 +0300

    Go func

---
+++ Go by Example/s06/01_func_hello/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	sayHi()
+	sayGreetings("ESP")
+	sayGreetings("GER")
+	sayGreetings("ENG")
+
+	fmt.Println()
+	fmt.Printf("Type of sayHi: %T\n", sayHi)
+	fmt.Printf("Type of sayGreetings: %T\n", sayGreetings)
+}
+func sayHi() {
+	fmt.Println("Hi there!")
+}
+
+func sayGreetings(lang string) {
+	if lang == "ESP" {
+		fmt.Println("Hola")
+	} else if lang == "GER" {
+		fmt.Println("Hallo")
+	} else {
+		fmt.Println("Hello")
+	}
+
+}
