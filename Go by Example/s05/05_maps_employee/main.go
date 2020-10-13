4d83b852a6ebecc67520366db31b7280735053d8
petros <pit.trak@gmail.com>
2020-10-13 12:00:33 +0300

    Go maps

---
+++ Go by Example/s05/05_maps_employee/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	employeeSalary := map[string]float64{
+		"Blake":  60000.00,
+		"Parker": 120000.50,
+		"Dakota": 93000.00,
+	}
+
+	fmt.Printf("Parker's Salary = %.2f\n", employeeSalary["Parker"])
+
+	salary1, ok := employeeSalary["Dakota"]
+	if ok {
+		fmt.Printf("Value %.2f was found.\n", salary1)
+	} else {
+		fmt.Printf("The specified key was not found.\n")
+	}
+
+	if salary2, ok := employeeSalary["Jordan"]; ok {
+		fmt.Printf("Value %.2f was found.\n", salary2)
+		delete(employeeSalary, "Jordan")
+	} else {
+		fmt.Printf("The specified key was not found.\n")
+	}
+
+	_, ok = employeeSalary["Dakota"]
+	if ok {
+		fmt.Printf("Key exists.\n")
+	} else {
+		fmt.Printf("Key doesn't exist.\n")
+	}
+
+	for name, salary := range employeeSalary { // key:name, value:salary
+		fmt.Println(name, salary)
+	}
+}
