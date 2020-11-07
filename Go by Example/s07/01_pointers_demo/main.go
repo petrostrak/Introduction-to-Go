44995f7484353cc50d9bf00ac001049f697aca2b
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-11-07 14:18:37 +0200

    Pointers in Go demo

---
+++ Go by Example/s07/01_pointers_demo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	p := &x
+	q := &x
+
+	fmt.Printf("x => type=%T, value=%d \n", x, x)
+	fmt.Printf("&x => type=%T, value=%d \n", &x, &x)
+	fmt.Println()
+	fmt.Printf("p => type=%T, value=%d \n", p, p)
+	fmt.Printf("*p => type=%T, value=%d \n", *p, *p)
+	fmt.Printf("&p => type=%T, value=%d \n", &p, &p)
+	fmt.Println()
+	fmt.Printf("q => type=%T, value=%d \n", q, q)
+	fmt.Printf("*q => type=%T, value=%d \n", *q, *q)
+	fmt.Printf("&q => type=%T, value=%d \n", &q, &q)
+}
