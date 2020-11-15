04293e307cb572866736c655329b1e41e8b85e96
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-11-15 14:31:16 +0200

    Comparing structs

---
+++ Go by Example/s09/05_struct_compare/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type book struct {
+	name, author string
+}
+
+func main() {
+	b1 := book{"Golang Head First", "Author 1"}
+	b2 := book{"Golang Head First", "Author 1"}
+	b3 := book{"Go Programming", "Author 2"}
+
+	fmt.Printf("LOC1 - %v\n", b1)
+
+	fmt.Println(compare1(b1, b2))
+	fmt.Println(compare1(b1, b3))
+	fmt.Printf("LOC2 - %v\n\n", b1)
+
+	fmt.Println(compare2(&b1, &b2))
+	fmt.Printf("LOC3 - %v\n\n", b1)
+
+	p1 := &book{"Go Web Programming", "Author 3"}
+
+	p2 := new(book)
+	*p2 = book{"Go Web Programming", "Author 3"}
+
+	fmt.Printf("LOC4 - &p1 = %v *p1 = %v\n", &p1, *p1)
+	fmt.Println(compare2(p1, p2))
+	fmt.Printf("LOC5 - &p1 = %v *p1 = %v\n", &p1, *p1)
+}
+
+func compare1(b book, q book) bool {
+	if b.author == q.author && b.name == q.name {
+		b.name = "Java Programming" // Doesn't change the original variable
+		return true
+	}
+	return false
+}
+
+func compare2(b *book, q *book) bool {
+	if b.author == q.author && b.name == q.name {
+		b.name = "Python Programming" // Change the original variable
+		return true
+	}
+	return false
+}
