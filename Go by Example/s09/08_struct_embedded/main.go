265a177fa1864e9577dd324855384339c9fcdb05
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-11-15 19:05:53 +0200

    Struct embedded

---
+++ Go by Example/s09/08_struct_embedded/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type generalInfo struct {
+	country, hairColor string
+}
+
+type player struct {
+	name, sport string
+	age         int
+	info        generalInfo
+}
+
+func main() {
+	var p1 player
+
+	p1.name = "Petros Trak"
+	p1.age = 34
+	p1.sport = "Coding"
+	p1.info.country = "Greece"
+	p1.info.hairColor = "Dark"
+
+	fmt.Println("P1 :", p1)
+
+	info2 := generalInfo{
+		country:   "Japan",
+		hairColor: "Blond",
+	}
+
+	p2 := player{
+		name:  "Eleni Apost",
+		age:   28,
+		sport: "Japanese",
+		info:  info2,
+	}
+
+	fmt.Println("P2 :", p2)
+}
