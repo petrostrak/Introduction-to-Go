c4660b58dc85d2c7e67565bb89b9b21f20001249
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-11-21 01:54:28 -0800

    The 'interface' Interfaces - Part 2

---
+++ Go by Example/s10/09_interface_sort_tostring/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type player struct {
+	Name    string
+	Country string
+}
+
+func (p player) String() string {
+	return fmt.Sprintf("toString() - %s: %s", p.Name, p.Country)
+}
+
+type byCountry []player
+
+func (c byCountry) Len() int           { return len(c) }
+func (c byCountry) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c byCountry) Less(i, j int) bool { return c[i].Country < c[j].Country }
+
+func main() {
+	p := []player{
+		{"Roger Federer", "Switzerland"},
+		{"Lionel Messi", "Argentina"},
+		{"Michael Jordan", "USA"},
+	}
+
+	fmt.Println(&p[0])
+	fmt.Println(p)
+
+	fmt.Println()
+	sort.Sort(byCountry(p))
+	fmt.Println(p)
+}
