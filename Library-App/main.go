2b42fca282d70e0dfe4c52b3fcf29b0629a35704
petrostrak <pit.trak@gmail.com>
2022-03-27 11:41:32 +0300

    Add returnBook

--- Library-App/main.go
+++ Library-App/main.go
@@ -92,6 +92,27 @@ func checkoutBook(library *Library, title Title, member *Member) bool {
 	return true
 }
 
+func returnBook(library *Library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book not part of the Library")
+		return false
+	}
+
+	audit, found := member.books[title]
+	if !found {
+		fmt.Println("Member did not check out this book")
+		return false
+	}
+
+	book.lended -= 1
+	library.books[title] = book
+
+	audit.checkIn = time.Now()
+	member.books[title] = audit
+	return true
+}
+
 func main() {
 
 }
